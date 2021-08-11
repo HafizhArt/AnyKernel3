585c2f120daad18d1e57f61adfe92dba3a865383
HafizhArt <52520555+HafizhArt@users.noreply.github.com>
2021-08-11 20:00:31 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=QuicksilveR Kernel by ghostrider-reborn
+kernel.string=Exciter by @notfound_304
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -15,7 +15,7 @@ device.name2=willow
 device.name3=
 device.name4=
 device.name5=
-supported.versions=10.0-11.0
+supported.versions=10.0-12.0
 supported.patchlevels=
 '; } # end properties
 
